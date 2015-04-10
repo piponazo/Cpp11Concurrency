92a5ce4d8531a175e30d7638784d796dba3b3dbc
Luis Diaz Mas <luis@pix4d.com>
2015-04-10 15:57:57 +0200

    new examples of the book

--- apps/chapter02/launch_threads.cpp
+++ apps/chapter02/launch_threads.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include <thread>
 
+using namespace std;
+
 void functionThread();
+
+class backgroundTask {
+public:
+    void operator()() const {
+        cout << "I'm the operator () of backgroundTask\n";
+    }
+};
 
 int main()
 {
-        std::thread threadFunction(functionThread);
+        backgroundTask task1;
+
+        thread threadFunction(functionThread);
+        thread threadClass1(task1);
+        thread threadClass2((backgroundTask()));
+        thread threadClass3{backgroundTask()};
+        thread threadLambda1([]{ cout << "I'm a lambda\n"; });
+
         threadFunction.join();
+        threadClass1.join();
+        threadClass2.join();
+        threadClass3.join();
+        threadLambda1.join();
         return EXIT_SUCCESS;
 }
 
 
 void functionThread()
 {
-        std::cout << "This is a simple function called with a thread" << std::endl;
+        cout << "This is a simple function called with a thread" << endl;
 }
---
+++ apps/chapter02/thread_guard.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+class ThreadGuard {
+public:
+    explicit ThreadGuard(thread &t) : _t(t) {}
+    ~ThreadGuard() {
+        if (_t.joinable()) {
+            _t.join();
+        }
+    }
+
+    ThreadGuard(const ThreadGuard &) = delete;
+    ThreadGuard & operator =(const ThreadGuard &) = delete;
+
+private:
+    thread &_t;
+};
+
+void functionThread(int & localVar)
+{
+    cout << "Value of variable: " << localVar << endl;
+}
+
+int main()
+{
+    int someLocalVar = 4;
+    thread t {functionThread, someLocalVar};
+    ThreadGuard g(t);
+    return EXIT_SUCCESS;
+}
